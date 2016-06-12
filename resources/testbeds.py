d9362eb53d91afe3f893ec41584111b0adfddaf8
Jesus Gavilan <jegavilgar@gmail.com>
2016-06-12 21:04:37 +0200

    added get testbed collection resource

--- resources/testbeds.py
+++ resources/testbeds.py
@@ -1,7 +1,10 @@
 __author__ = 'jesus.gavilan'
 import logging
 import falcon
-
+from bson import Binary, Code
+from bson.json_util import dumps,loads
+from mapper.testbeddb import TestbedMapper
+from utils.jsonmsg import *
 """
 Testbeds Collection resource
 """
@@ -14,14 +17,25 @@ class TestbedsCollection(object):
         TO-DO
         :return:
         """
-
         self.logger = logging.getLogger(__name__)
 
     def on_get(self, req, resp):
         """
         GET testbed collection resource
-        TO-DO
+        :param req:HTTP request of testbed collection resource
+        :param resp: HTTP response of the testbed collection resource
         """
+        try:
+            # Get all testbed collection data and parsed to JSON
+            tbList = TestbedMapper().getAll()
+            success_msg = success
+            success_msg["data"] = tbList
+            # Send 200 OK response with the tesbed collection data
+            resp.status = falcon.HTTP_200
+            resp.body = dumps(success_msg)
+        except Exception as ex:
+            self.logger.error("Error getting testbed collection. Info: ", ex)
+            raise falcon.HTTPError(falcon.HTTP_500, description="DB Error")
 
 """
 Testbeds Item resource
